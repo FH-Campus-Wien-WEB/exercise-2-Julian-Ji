ecc68a092c589a77c304b2374b4436772858c007
Julian Ji <JulianJi@outlook.de>
2026-04-20 20:56:32 +0200

    ex1 code input

--- server/server.js
+++ server/server.js
@@ -15,8 +15,49 @@ app.use(express.static(path.join(__dirname, 'files')));
 app.get('/movies', function (req, res) {
   /* Task 1.2. Remove the line below and eturn the movies from 
      the model as an array */
-  res.sendStatus(404)
-})
+  const myMovies = [
+    {
+      "Title": "The Martian",
+      "Released": "2015-10-02",
+      "Runtime": 144,
+      "Genres": ["Adventure", "Drama", "Sci-Fi"],
+      "Directors": ["Ridley Scott"],
+      "Writers": ["Drew Goddard", "Andy Weir"],
+      "Actors": ["Matt Damon", "Jessica Chastain", "Kristen Wiig"],
+      "Plot": "An astronaut becomes stranded on Mars after his team assumes him dead...",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SX300.jpg",
+      "Metascore": 80,
+      "imdbRating": 8.0
+    },
+    {
+      "Title": "Whiplash",
+      "Released": "2014-10-15",
+      "Runtime": 106,
+      "Genres": ["Drama", "Music"],
+      "Directors": ["Damien Chazelle"],
+      "Writers": ["Damien Chazelle"],
+      "Actors": ["Miles Teller", "J.K. Simmons", "Melissa Benoist"],
+      "Plot": "A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMDFjOWFkYzktYzhhMC00NmYyLTkwY2EtYjViMDhmNzg0OGFkXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 89,
+      "imdbRating": 8.5
+    },
+    {
+      "Title": "Star Wars: Episode IV - A New Hope",
+      "Released": "1977-05-25",
+      "Runtime": 121,
+      "Genres": ["Action", "Adventure", "Fantasy"],
+      "Directors": ["George Lucas"],
+      "Writers": ["George Lucas"],
+      "Actors": ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+      "Plot": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station...",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BOGUwMDk0Y2MtNjBlNi00NmRiLTk2MWYtMGMyMDlhYmI4ZDBjXkEyXkFqcGc@._V1_SX300.jpg",
+      "Metascore": 90,
+      "imdbRating": 8.6
+    }
+  ];
+  res.json(myMovies);
+});
 
 // Configure a 'get' endpoint for a specific movie
 app.get('/movies/:imdbID', function (req, res) {
